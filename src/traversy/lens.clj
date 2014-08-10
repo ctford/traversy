697853b2a9ba205380bd658e6608ea5d7879e742
Chris Ford <christophertford@gmail.com>
2014-08-10 16:31:34 +0100

    Refactor away from lambda notation.

--- src/traversy/lens.clj
+++ src/traversy/lens.clj
@@ -7,17 +7,13 @@
 (defrecord SingleFocus
   [focus fmap]
   Lens
-  (focus [this x] (focus x))
-  (fmap [this f x] (fmap f x)))
+  (focus [_ x] (focus x))
+  (fmap [_ f x] (fmap f x)))
 
 (defn view [x lens] (focus lens x))
 (defn update [x lens f] (fmap lens f x))
 
-(defn fidentity [f x] (f x))
-(def it (->SingleFocus identity fidentity))
-
+(def it (->SingleFocus identity (fn [f x] (f x))))
 (def each (->SingleFocus seq map))
-
-(defn in [path] (->SingleFocus #(get-in % path) #(update-in %2 path %1)))
-
-(def elements (->SingleFocus seq #(->> %2 seq (map %1) set)))
+(defn in [path] (->SingleFocus (fn [x] (get-in x path)) (fn [f x] (update-in x path f))))
+(def elements (->SingleFocus seq (fn [f x] (->> x seq (map f) set))))
