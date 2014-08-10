7405682e326ecc908b6578c08fa008e1572cd288
Chris Ford <christophertford@gmail.com>
2014-08-10 16:16:03 +0100

    Directly use the record constructor.

--- src/traversy/lens.clj
+++ src/traversy/lens.clj
@@ -10,10 +10,8 @@
   (focus [this x] (focus x))
   (fmap [this f x] (fmap f x)))
 
-(defn lens [focus fmap] (->SingleFocus focus fmap))
-
-(defn view [x l] (focus l x))
-(defn update [x l f] (fmap l f x))
+(defn view [x lens] (focus lens x))
+(defn update [x lens f] (fmap lens f x))
 
 (defn fidentity [f x] (f x))
-(def id (lens identity fidentity))
+(def id (->SingleFocus identity fidentity))
