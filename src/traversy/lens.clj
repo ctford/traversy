c8d1540d648d86b8062f080b27a980a78229f958
Chris Ford <christophertford@gmail.com>
2014-08-10 16:12:27 +0100

    Define the identity lens.

--- src/traversy/lens.clj
+++ src/traversy/lens.clj
@@ -1,1 +1,19 @@
 (ns traversy.lens)
+
+(defprotocol Lens
+  (focus [this x])
+  (fmap [this f x]))
+
+(defrecord SingleFocus
+  [focus fmap]
+  Lens
+  (focus [this x] (focus x))
+  (fmap [this f x] (fmap f x)))
+
+(defn lens [focus fmap] (->SingleFocus focus fmap))
+
+(defn view [x l] (focus l x))
+(defn update [x l f] (fmap l f x))
+
+(defn fidentity [f x] (f x))
+(def id (lens identity fidentity))
--- test/traversy/test/lens.clj
+++ test/traversy/test/lens.clj
@@ -1,3 +1,7 @@
 (ns traversy.test.lens
   (:require [midje.sweet :refer :all]
             [traversy.lens :refer :all]))
+
+(fact "The identity lens is non-prescription."
+  (-> 9 (view id)) => 9
+  (-> 9 (update id inc)) => 10)
