cb3c4897b1ee917cf5712220cdc147698fb397d1
Chris Ford <christophertford@gmail.com>
2014-08-11 21:13:42 +0100

    Refactor some constituent functors.

--- src/traversy/lens.clj
+++ src/traversy/lens.clj
@@ -19,28 +19,28 @@
 
 (def each (lens seq map))
 (def eachv (lens seq mapv))
-(def elements (lens seq (fn [f x] (->> x (map f) set))))
+
+(defn map-s [f x] (->> x (map f) set))
+(def elements (lens seq map-s))
 
 (defn fnth [n f [x & xs]]
   (if (zero? n)
     (cons (f x) xs)
     (cons x (fnth (dec n) f xs))))
-
 (defn xth [n] (lens (comp list #(nth % n)) (partial fnth n)))
 
 (defn fapply-in [path f x] (update-in x path f))
 (defn in [path] (lens (fn [x] (list (get-in x path))) (partial fapply-in path)))
 
-(def all-entries (lens seq (fn [f x] (->> x (map f) (reduce conj {})))))
-
-(defn fwhen [applicable? f x] (if (applicable? x) (f x) x))
-(defn fmap-when [applicable? f x] (map (partial fwhen applicable? f) x))
-(defn only [applicable?] (lens (fn [x] (filter applicable? x)) (partial fmap-when applicable?)))
+(defn fwhen [applicable? f x] (map (fn [e] (if (applicable? e) (f e) e)) x))
+(defn only [applicable?] (lens (partial filter applicable?) (partial fwhen applicable?)))
 
 (defn combine [outer inner]
   (lens
     (fn [x] (mapcat (partial focus inner) (focus outer x)))
     (fn [f x] (fmap outer (partial fmap inner f) x))))
 
+(defn map-m [f x] (->> x (map f) (reduce conj {})))
+(def all-entries (lens seq map-m))
 (def all-values (combine all-entries (in [1])))
 (def all-keys (combine all-entries (in [0])))
