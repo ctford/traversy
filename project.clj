389f73fc29f33e8ca7f2bb545911195aeb69c3b3
Chris Ford <christophertford@gmail.com>
2014-08-28 22:28:16 +0100

    Update licence reference in project.clj.

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject traversy "0.1.0-SNAPSHOT"
   :description "Lenses and multilenses."
   :url "https://github.com/ctford/traversy"
-  :license {:name "BSD 3-clause"
-            :url "http://opensource.org/licenses/BSD-3-Clause"}
+  :license {:name "MIT"
+            :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.5.1"]]
   :profiles {:dev
              {:plugins [[lein-midje "3.1.3"]]
