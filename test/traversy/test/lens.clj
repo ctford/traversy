920384b2f04b0472f9cbe795eb922d98c23c7c06
Chris Ford <christophertford@gmail.com>
2014-08-30 10:38:53 +0100

    Fix old reference to view-all.

--- test/traversy/test/lens.clj
+++ test/traversy/test/lens.clj
@@ -48,9 +48,9 @@
   (-> {:foo 1 :bar 2} (update all-keys {:foo :frag :bar :barp})) => {:frag 1 :barp 2})
 
 (fact "The 'maybe' lens focuses only on foci that are present."
-  (-> {:foo 1} (view-all (*> (in [:foo]) maybe))) => [1]
-  (-> {:foo 1} (view-all (*> (in [:bar]) maybe))) => []
-  (-> {:foo 1} (view-all (*> (+> (in [:foo]) (in [:bar])) maybe))) => [1])
+  (-> {:foo 1} (view (*> (in [:foo]) maybe))) => [1]
+  (-> {:foo 1} (view (*> (in [:bar]) maybe))) => []
+  (-> {:foo 1} (view (*> (+> (in [:foo]) (in [:bar])) maybe))) => [1])
 
 (fact "The 'only' lens focuses on the items in a sequence matching a condition."
   (-> [1 2 3] (view (only even?))) => [2]
