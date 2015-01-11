055bb65816951c123f72fafac04e18693c1eb052
Chris Ford <christophertford@gmail.com>
2015-01-11 14:40:56 +0000

    Add Codox for documenting the API.

--- project.clj
+++ project.clj
@@ -5,5 +5,7 @@
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.5.1"]]
   :profiles {:dev
-             {:plugins [[lein-midje "3.1.3"]]
-              :dependencies [[midje "1.6.3"]]}})
+             {:plugins [[lein-midje "3.1.3"]
+                        [codox "0.8.10"]]
+              :dependencies [[midje "1.6.3"]]}}
+  :codox {:src-dir-uri "http://github.com/ctford/traversy/blob/0.3.0/"})
