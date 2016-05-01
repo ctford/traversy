09feffc0a91304d6742ef9d9d9c3dcf0b5423805
John Cowie <john@imin.co>
2016-05-01 21:16:12 +0100

    Added deflenses macro

---
+++ src/traversy/macros.clj
@@ -0,0 +1,49 @@
+(ns traversy.macros
+  (:require [traversy.lens :as l]))
+
+(defn get-paths [n id?]
+  (cond (id? n) [[[:val n]]]
+        (map? n) (mapcat (fn [[k v]] (map (partial cons [:key k]) (get-paths v id?))) n)
+        (sequential? n) (apply concat (map-indexed (fn [k v] (map (partial cons [:index k]) (get-paths v id?))) n))
+        :else []))
+
+(defn combine-paths [paths]
+  (if (empty? (first paths))
+    []
+    (let [heads (map first paths)]
+      (->
+        (cond
+          (apply = heads) (first heads)
+          (= (ffirst heads) :key) [:multi-key]
+          (= (ffirst heads) :index) [:multi-index]
+          :else (throw (Exception. "Don't know what to do")))
+        (cons (combine-paths (map rest paths)))))))
+
+(defn stage-to-lens [[type key]]
+  (case type
+    :key (l/in [key])
+    :index (l/xth key)
+    :multi-key l/all-values
+    :multi-index l/each
+    (throw (Exception. (str "Unknown stage: " type)))))
+
+(defn path-to-lens [p]
+  (->> p
+       drop-last
+       (map stage-to-lens)
+       (apply l/*>)))
+
+(defn convert-path-group-to-lens-entry [[k v]]
+  [(second k)
+   (-> v combine-paths path-to-lens)])
+
+(defn gen-lenses [m id?]
+  (->> (get-paths m id?)
+       (group-by last)
+       (map convert-path-group-to-lens-entry)
+       (into {})))
+
+(defmacro deflenses [m]
+  `(do
+     ~@(for [[s l] (gen-lenses m symbol?)]
+         `(def ~s ~l))))
---
+++ test/traversy/test/macros.clj
@@ -0,0 +1,77 @@
+(ns traversy.test.macros
+  (:require [clojure.test :refer [deftest is testing are]]
+            [traversy.macros :as m]
+            [traversy.lens :as l]))
+
+(deftest test-lens-paths
+  (testing "can break map into paths"
+    (is
+      (=
+        (m/get-paths {:a 1 :b [{:c 2 :d 3} 4]} integer?)
+        [[[:key :a] [:val 1]]
+         [[:key :b] [:index 0] [:key :c] [:val 2]]
+         [[:key :b] [:index 0] [:key :d] [:val 3]]
+         [[:key :b] [:index 1] [:val 4]]]))))
+
+(deftest test-combining-paths
+  (testing "combining paths"
+    (is (=
+          (m/combine-paths
+            [[[:key :a] [:index 0] [:key :b]]
+             [[:key :a] [:index 1] [:key :b]]])
+          [[:key :a] [:multi-index] [:key :b]]))
+    (is (=
+          (m/combine-paths
+            [[[:key :a] [:key :b]]
+             [[:key :c] [:key :b]]])
+          [[:multi-key] [:key :b]]))
+    (is (=
+          (m/combine-paths
+            [[[:key :a]]])
+          [[:key :a]]))))
+
+(defn lens= [data lens expected-value]
+  (= (l/view data lens) expected-value))
+
+(deftest test-gens-lens
+  (testing "generating lens map"
+        (let [lens-defs {:a "->a"
+                         :b {:c "->c"}
+                         :c {:c1 ["->c11" "->c12"]}}
+              data {:a "A"
+                    :b {:c "C"}
+                    :c {:c1 ["C1" "C2"]}}
+              lens-m (m/gen-lenses lens-defs string?)]
+          (are [lens-id expected-value] (lens= data (get lens-m lens-id) expected-value)
+               "->a"   ["A"]
+               "->c"   ["C"]
+               "->c11" ["C1"]
+               "->c12" ["C2"]))))
+
+(m/deflenses {:a ->a
+              :b {:c ->c}
+              :c {:c1 [->c11 ->c12]}
+              :d {:a {:x [->x1 ->x2] :y ->y :z ->z}
+                  :b {:x [->x1 ->x2] :y ->y :z ->z}}
+              :e [{:p ->ep} {:p ->ep}]
+              :f "bob"})
+
+(deftest test-deflenses
+  (testing "the deflenses macro"
+    (let [data {:a "A"
+                :b {:c "C"}
+                :c {:c1 ["C1" "C2"]}
+                :d {:a {:x [1 2] :y "AY" :z "AZ"}
+                    :b {:x [11 12] :y "BY" :z "BZ"}}
+                :e [{:p 123} {:p 456}]}]
+      (are [lens expected-value] (lens= data lens expected-value)
+           ->a    ["A"]
+           ->c    ["C"]
+           ->c11  ["C1"]
+           ->c12  ["C2"]
+           ->y    ["AY" "BY"]
+           ->z    ["AZ" "BZ"]
+           ->x1   [1 11]
+           ->x2   [2 12]
+           ->ep   [123 456]
+       ))))
